f2b5786c490c8d91f995ace0323c38c4803d7a17
igorchagov <igorchagov@mail.ru>
2021-10-20 11:11:27 +0300

    'Спам'

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,6 @@
 function checkSpam(str) {
-  // ваш код...
-}
+  let low = str.toLowerCase();
+             return low.includes('1xbet') || low.includes('xxx');
+                }
+           console.log (checkSpam('1XbEt'));
+
