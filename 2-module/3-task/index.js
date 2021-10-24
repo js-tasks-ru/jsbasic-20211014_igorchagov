69577d73189add1b66146ac6f64ee73a9afb95d9
igorchagov <igorchagov@mail.ru>
2021-10-24 23:11:14 +0300

    'калькулятор'

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,18 @@
 let calculator = {
-  // ваш код
+  read(a, b){ 
+    this.c = a;
+    this.d = b;
+    return this.c, this.d;         
+},
+  sum(){
+    return +this.c + +this.d;
+},
+mul(){
+    return +this.c * +this.d;
+},
 };
-
+calculator.read(3,5);
+console.log(calculator.sum()); 
+console.log(calculator.mul());
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально
