3d66a532bc3f9b8e7e01f00b2698789fe42cdcce
igorchagov <igorchagov@mail.ru>
2021-10-26 17:09:33 +0300

    'фильтр'

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,7 @@
+let arr = [5, 3, 8, 1];
 function filterRange(arr, a, b) {
-  // ваш код...
-}
+  let filter=arr.filter(item =>(a <=item && item <=b));
+  return filter;
+};
+let filtered = filterRange(arr, 1, 4);
+console.log(filtered);
