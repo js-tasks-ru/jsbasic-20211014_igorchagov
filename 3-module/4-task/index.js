0e38b5ecadd3fa9390f836bddcb6d35e10c76f0b
igorchagov <igorchagov@mail.ru>
2021-10-26 18:48:54 +0300

    'зарплатаИмя'

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,11 @@
 function showSalary(users, age) {
-  // ваш код...
+  let names = [];
+  users.forEach((user) => {
+  if (user.age <= age) {
+  names.push(`${user.name}, ${user.balance}`);
 }
+});                  
+let a = names.join('\n');
+  return a;
+};
+
