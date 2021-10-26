25bb740507d2e16ee6fb219852efd5e108c15d59
igorchagov <igorchagov@mail.ru>
2021-10-26 17:52:11 +0300

    'перевод'

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,11 @@
+let str='background-color';
 function camelize(str) {
-  // ваш код...
-}
+  let str1 = str.split('-');
+  let str2 = str1.map(
+    (word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1)
+  );
+  let str3 = str2.join(''); 
+  return str3;
+};
+let a = camelize(str);
+console.log(a);
