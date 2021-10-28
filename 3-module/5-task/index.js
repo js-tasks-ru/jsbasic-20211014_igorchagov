296845ba6c232c98ba285bd8831a8b560153b265
igorchagov <igorchagov@mail.ru>
2021-10-28 20:19:59 +0300

    'МинМакс'

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,12 @@
 function getMinMax(str) {
-  // ваш код...
-}
+  let arr = str.split(' ').map(Number);
+arr = arr.filter(x => !Number.isNaN(x));
+console.log(arr);
+  let result ={ 
+  'min':'' ,
+  'max': '',
+  };
+  result['min'] = Math.min.apply(0, arr);
+  result['max'] = Math.max.apply(0, arr);
+  return result;
+};
