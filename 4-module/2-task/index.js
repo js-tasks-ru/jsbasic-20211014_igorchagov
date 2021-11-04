a097ae97908e6f984086f3b9a855e89d149903ea
igorchagov <igorchagov@mail.ru>
2021-11-04 20:12:07 +0300

    'таблица'

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,14 @@
-function makeDiagonalRed(table) {
-  // ваш код...
-}
+let body = document.body;
+let children = body.children;
+let table = children['1'];
+console.log(table);
+  function makeDiagonalRed(table) {
+   let row = 0;
+   for (let i = 0; i < table.rows.length; i++) {
+      row = table.rows[i]
+      row.cells[i].style.backgroundColor = 'red';
+  }
+    return row;
+  };
+console.log(makeDiagonalRed(table))
+ 
