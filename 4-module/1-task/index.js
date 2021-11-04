552640d7817a4509d96f5739e2b3806562c22793
igorchagov <igorchagov@mail.ru>
2021-11-04 19:12:37 +0300

    'хтмлСписок'

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,10 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let friendsNames=[];
+  for(let name of friends){
+   friendsNames.push(`<li>${name.firstName}  ${name.lastName}</li>`);
+  };
+  let a = friendsNames.join('\n');
+  let ul = document.createElement('ul');
+  ul.innerHTML = a;
+  return ul;
 }
